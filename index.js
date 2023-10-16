8a0a27ef7aeaa7f05fbe9defd651f5ceba2a5cdc
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-10-16 17:44:17 +0500

    Implement Create Blog Functionality

--- index.js
+++ index.js
@@ -7,27 +7,33 @@ const { checkForAuthCookie } = require('./middlewares/auth')
 const app = express()
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
+const Blog = require('./models/blog')
 // connect mongodb
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((e) => {
     console.log("MongoDB Connected")
 })
 
 const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static(path.resolve('./public')))
 app.use(checkForAuthCookie('token'))
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
 //Routes
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find({})
     res.render('index', {
         user: req.user,
+        blogs: allBlogs
     })
 })
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
 
 app.listen(PORT, () => {
