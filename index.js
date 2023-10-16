09bb299eebf29226c557c77dfa9b7fc9d265172b
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-10-16 07:21:01 +0500

    Create Signin and signup functionality

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+// connect mongodb
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((e) => {
+    console.log("MongoDB Connected")
+})
+
+const userRoute = require('./routes/user')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.use('/user', userRoute)
 
 
 app.listen(PORT, () => {
