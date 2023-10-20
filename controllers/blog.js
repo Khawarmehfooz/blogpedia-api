65b98440a542663dcd820fd13362ae1585a1b62d
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-10-20 08:44:14 +0500

    update: nest statement in try-catch block

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,36 +1,64 @@
 const Blog = require('../models/blog')
 async function handleCreateBlogPost(req, res) {
-    const { title, body } = req.body
-    const blog = await Blog.create({
-        title,
-        body,
-        coverImageURL: '/uploads/blog.png'
-    })
-    res.send('Post Created')
+    try {
+        const { title, body } = req.body
+        const blog = await Blog.create({
+            title,
+            body,
+            coverImageURL: '/uploads/blog.png'
+        })
+        res.send('Post Created')
+    } catch (err) {
+        console.log(err)
+        res.status(400).send('Server Error')
+    }
 }
+
 async function handleGetAllPosts(req, res) {
-    const allPosts = await Blog.find({})
-    res.json(allPosts)
+    try {
+        const allPosts = await Blog.find({})
+        res.json(allPosts)
+    } catch (err) {
+        console.log(err)
+        res.status(400).send('Server Error')
+    }
 }
 async function handleGetPostById(req, res) {
-    const id = req.params.id
-    const blogPost = await Blog.findById(id)
-    res.json(blogPost)
+    try {
+        const id = req.params.id
+        const blogPost = await Blog.findById(id)
+        res.json(blogPost)
+    } catch (err) {
+        res.status(400).send('Server Error')
+    }
 }
+
 async function handleUpdatePost(req, res) {
-    const id = req.params.id
-    const { title, body } = req.body
-    await Blog.findByIdAndUpdate(id, {
-        title,
-        body
-    })
-    res.send('Post Updated')
+    try {
+
+        const id = req.params.id
+        const { title, body } = req.body
+        await Blog.findByIdAndUpdate(id, {
+            title,
+            body
+        })
+        res.send('Post Updated')
+    } catch (err) {
+        console.log(err)
+        res.status(500).json('Server Error')
+    }
 }
+
 async function handleDeleteBlogPost(req, res) {
-    const id = req.params.id
-    await Blog.findByIdAndDelete(id)
-    res.send('Post Deleted')
+    try {
+        const id = req.params.id
+        await Blog.findByIdAndDelete(id)
+        res.send('Post Deleted')
+    } catch (err) {
+        res.status(400).json(`Error: ${err}`)
+    }
 }
+
 module.exports = {
     handleCreateBlogPost,
     handleGetAllPosts,
